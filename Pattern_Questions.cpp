a29487cf1ff6e1bc2ffc19a7be67b9d0b178995e
abhikot1996 <79213991+abhikot1996@users.noreply.github.com>
2021-08-25 12:25:16 +0530

    25/08/2021, 12:26 PM

--- Pattern_Questions.cpp
+++ Pattern_Questions.cpp
@@ -525,24 +525,24 @@
 //     return 0;
 // }
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     int no;
-//     cout<<"Enter no: ";
-//     cin>>no;
-//     for(int r=1;r<=no;r++)
-//     {
-//         for(int c=1;c<=no*4;c++)
-//         {
-//             if(c==(no+1)-r||c==(no-1)+r||c==(no*3)-1-r||c==no*3-3+r)
-//             cout<<"* ";
-//             else
-//             cout<<"  ";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+int main()
+{
+    int no;
+    cout<<"Enter no: ";
+    cin>>no;
+    for(int r=1;r<=no;r++)
+    {
+        for(int c=1;c<=no*4;c++)
+        {
+            if(c==(no+1)-r||c==(no-1)+r||c==(no*3)-1-r||c==no*3-3+r)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
