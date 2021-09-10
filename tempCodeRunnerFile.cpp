d4e4e1e824fbc0711a1fa2b7aea9b635d10cb415
abhikot1996 <79213991+abhikot1996@users.noreply.github.com>
2021-09-10 06:57:23 +0530

    10/09/2021, 06:58 AM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-;loop<=NoOfKids;loop++)
-    // {
-    //     if(GNoOfCandies<NoOfCandies[loop])
-    //     {   GNoOfCandies=NoOfCandies[loop];
-    //         GNameOfKid=NameOfKids[loop];}
-    // }
-    // cout<<"Kid with greatest no of Candies,"<<endl;
-    // cout<<"Name: "<<GNameOfKid<<endl;
-    // cout<<"No of candies: "<<GNoOfCandies<<endl;
+{   for(int c=a;c<=b;c++)
